6ee26e3c8ae0c39b48ebc6ea429dd4ae21d7e45d
sunyy <18437929553@163.com>
2020-10-13 15:18:54 +0800

    vue 学习代码提交

---
+++ vue.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    // 公共路径(必须有的)
+    publicPath: "./",
+    // 输出文件目录
+    outputDir: "dist",
+    // 静态资源存放的文件夹(相对于ouputDir)
+    assetsDir: "static",
+    // eslint-loader 是否在保存的时候检查(果断不用，这玩意儿我都没装)
+    lintOnSave: false,
+    // 我用的only，打包后小些
+    runtimeCompiler: false,
+    productionSourceMap: true, // 不需要生产环境的设置false可以减小dist文件大小，加速构建
+    devServer: {
+        open: false,  // npm run serve后自动打开页面
+        host: '0.0.0.0',  // 匹配本机IP地址(默认是0.0.0.0)
+        port: 8081, // 开发服务器运行端口号
+        proxy: null,
+    },
+
+    /*引入less*/
+    css: {
+        loaderOptions: {
+            less: {
+                lessOptions: {
+                    modifyVars: {
+                        'primary-color': '#1890FF',
+                        'link-color': '#1890FF',
+                        'border-radius-base': '2px',
+                    },
+                    javascriptEnabled: true,
+                },
+            },
+        },
+    },
+
+}
